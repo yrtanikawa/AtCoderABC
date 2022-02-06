29a37586fb6b0b360018ebf6b290526c5825f3e7
yrtanikawa <tanikawayuri1020@gmail.com>
2022-02-06 11:02:09 +0900

    add

--- 20220130ABC237_absent/MainC.java
+++ 20220130ABC237_absent/MainC.java
@@ -2,7 +2,23 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
+		String s = sc.next();
+		int i = 0;
+		int n = s.length()-1;
+		while(i<n && s.charAt(n)=='a'){
+			n--;
+			if(s.charAt(i)=='a'){
+				i++;
+			}
+		}
+		int size = n;
+		for(int j = i; j <= n; j++){
+			if(s.charAt(j)!=s.charAt(size)){
+				System.out.println("No");
+				return;
+			}
+			size--;
+		}
+		System.out.println("Yes");
 	}
 }
